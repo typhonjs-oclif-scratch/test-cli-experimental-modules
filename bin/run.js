06ba404c5a23abbb5ed2ae1cce8eca7bf4986c36
typhonrt <support@typhonrt.org>
2021-05-29 19:40:27 -0700

    update bin

--- bin/run.js
+++ bin/run.js
@@ -1,9 +1,7 @@
 #!/usr/bin/env sh
 // 2>/dev/null; exec /usr/bin/env node --experimental-modules "$0" "$@"
-import url   from 'url';
-import flush from '@oclif/core/flush.js';
 import oclif from '@oclif/core';
 
-oclif.run(void 0, url.fileURLToPath(import.meta.url))
-.then(flush)
+oclif.run(void 0, import.meta.url)
+.then(oclif.flush)
 .catch(oclif.Errors.handle);
